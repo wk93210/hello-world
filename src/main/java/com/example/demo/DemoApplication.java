5923313e16dcdc8eddf3784dc09e7f39c3ff0b3f
Lunan Li <wk93210@gmail.com>
2022-11-13 01:47:08 +0000

    fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+	@RequestMapping("/health")
+	public void health() {}
+
 	@RequestMapping("/")
 	public String home() {
 		return "Hello Docker World";
