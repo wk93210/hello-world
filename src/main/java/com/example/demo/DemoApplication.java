33ffa36e60ec4c598ae2f9c350ad4005c32f45eb
Lunan Li <wk93210@gmail.com>
2022-11-13 09:14:04 +0000

    redirect

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
+
 
 @SpringBootApplication
 @RestController
@@ -12,8 +16,13 @@ public class DemoApplication {
 	@RequestMapping("/health")
 	public void health() {}
 
-	@RequestMapping("/")
-	public void home() {}
+	@RequestMapping(
+		value = {"redirect"},
+		method = RequestMethod.PUT)
+    @ResponseBody
+    public RedirectView redirectConversionFilesPutRequest() {
+        return new RedirectView("https://www.google.com");
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
